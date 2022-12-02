67e8e2a8ac0e85e60225623db325d29f8f1eb046
bigBadMatt <5707@holbertonstudents.com>
2022-12-01 21:21:27 -0800

    implemented main while loop

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "shell.h"
+
+int shell()
+{
+	int num = 0;
+	char *buff;
+	size_t buffsize;
+
+	buffsize = 32;
+	buff = malloc(buffsize * sizeof(char));
+	if (buff == NULL)
+	{
+		return (-1);
+	}
+	while (num == 0)
+	{
+		;
+	}
+	return (0);
+
+}
 
 /**
  *
@@ -8,5 +29,6 @@
 */
 int main()
 {
+	shell();
 	return(0);
 }
