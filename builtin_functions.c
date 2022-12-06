8c1fa39876adb4f88e6b0d6e482376bb09180ec4
bigBadMatt <5707@holbertonstudents.com>
2022-12-05 20:08:18 -0800

    fixed bugs to get program to compile

---
+++ builtin_functions.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+
+int shell_cd()
+{
+	;
+}
+
+int shell_help()
+{
+	;
+}
+
+int shell_exit()
+{
+	putchar('y');
+	exit(0);
+}
+
--- exec.c
+++ exec.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
+#include "shell.h"
 
-int _execve(const char *path, char *const argv[], char *const envp[])
+int _execve(char *path, char *argv[], char *envp[])
 {
 		pid_t child_pid, parent_pid;
 		int status;
