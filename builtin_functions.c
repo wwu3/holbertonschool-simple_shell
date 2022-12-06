4d7881f208bb124c1d05661ffe418afc99ac828a
pallavi149 <5811@holbertonschoolstudents.com>
2022-12-05 20:26:57 -0800

    Implementing CD function

--- builtin_functions.c
+++ builtin_functions.c
@@ -1,9 +1,29 @@
 #include "shell.h"
 
+/**
+ * shell_cd - implementing cd system call
+ * @pth: path of directory
+ * Return: -1 if function fails
+ */
 
-int shell_cd()
+int shell_cd(char *pth)
 {
-	;
+    char path[200];
+    char cwd[200];
+
+    strcpy(path,pth);
+    if(pth[0] != '/')
+    {// true for the dir in cwd
+        getcwd(cwd, sizeof(cwd));
+        strcat(cwd, "/");
+        strcat(cwd, path);
+        chdir(cwd);
+    }
+    else
+    {//true for dir w.r.t. /
+        chdir(pth);
+    }
+    return (0);
 }
 
 int shell_help()
@@ -16,4 +36,3 @@ int shell_exit()
 	putchar('y');
 	exit(0);
 }
-
