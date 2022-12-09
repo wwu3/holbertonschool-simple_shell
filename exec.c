38307890af93a1200a41f5fb181c16e60d0287af
wendy <5703@holbertonstudents.com>
2022-12-08 22:38:07 -0800

    fixed betty error

--- exec.c
+++ exec.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * _execve - allows to execute another program
+ * @path: the path of the file for execution of a new process
+ * @argv: the name of the editable file
+ * @env: the environment list
+ * Return: 0 if succeeds, -1 if it's eof
+ */
 int _execve(__attribute__((unused)) char *path, char *argv[], char **env)
 {
 		pid_t child_pid;
